1fbf9f7ae7933a7f6cd39754eda1433c4b5071af
katieburrows <katie_burrows@me.com>
2018-11-07 16:00:25 -0700

    removed pseudo code from js file

--- game.js
+++ game.js
@@ -1,26 +1,3 @@
-// <!-- message that says "press r, p, or s to start" -->
-// <!-- once r, p, or s is clicked get rid of that message and load: -->
-//     <!-- you chose: (insert the user's keystroke, but only if it's r, p, or s) -->
-//     <!-- the computer chose: (the computer's random choice) -->
-//     <!-- wins counter (set to 0)-->
-//     <!-- losses counter (set to 0)-->
-//     <!-- ties counter (set to 0)-->
-// <!-- grab the user's choice and trigger logic that chooses a random computer letter -->
-// <!-- compare the user's choice with the computer's -->
-
-//     <!-- if computer's choice is r and the user's choice is s losses++-->
-//      <!-- if computer's choice is s and the user's choice is p losses++-->
-//     <!-- if computer's choice is p and the user's choice is r losses++-->
-
-    // <!-- if computer's choice is r and the user's choice is p wins++-->
-//     <!-- if computer's choice is p and the user's choice is s wins++-->
-//     <!-- if computer's choice is s and the user's choice is r wins++-->
-//     
-//     <!-- catch-all bucket for ties in else, no comparison needed -->
-
-// <!-- reselect the computer's choice but only display it after the user has selected -->
-// <!-- retrigger game, add to w/l/t counters -->
-
 document.write("Press r, p, or s to start");
 
 var choices = ["r", "p", "s"]
