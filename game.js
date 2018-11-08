5e7c9715bfeef6d70384136439412e2d4dc9983a
katieburrows <katie_burrows@me.com>
2018-11-07 20:46:52 -0700

    added logic to make sure the proper keys were being played and added in more comments

--- game.js
+++ game.js
@@ -14,22 +14,25 @@ document.onkeyup = function(event){
         var compChoice = choices[Math.floor(Math.random() * choices.length)];
         console.log("computer choice: " + compChoice);
 
-        //these conditionals compare the user's guess to the computer's guess and set up the rules for wins/losses/ties and then adds to the appropriate counter depending on the outcome.
+        //this checks to make sure that the player is using the appropriate keys.
+        if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")){
 
-        //this is the losses bucket.
-        if (compChoice === "r" && userGuess === "s" || compChoice === "s" && userGuess === "p" || compChoice === "p" && userGuess === "r"){
-            losses++;
-        } 
+            //these conditionals compare the user's guess to the computer's guess and set up the rules for wins/losses/ties and then adds to the appropriate counter depending on the outcome.
 
-        //this is the wins bucket.
-        else if (compChoice === "r" && userGuess === "p" || compChoice === "p" && userGuess === "s" || compChoice === "s" && userGuess === "r"){
-            wins++;
-        } 
+            //this is the losses bucket.
+            if (compChoice === "r" && userGuess === "s" || compChoice === "s" && userGuess === "p" || compChoice === "p" && userGuess === "r"){
+                losses++;
+            } 
 
-        //this is the ties bucket.  It doesn't contain conditions similar to above because there are only 3 buckets that can be fallen into--you either fall into the wins bucket or the losses bucket, if you make it past that then the only option left is that it was a tie.
-        else {
-            ties ++;
-        }
+            //this is the wins bucket.
+            else if (compChoice === "r" && userGuess === "p" || compChoice === "p" && userGuess === "s" || compChoice === "s" && userGuess === "r"){
+                wins++;
+            } 
+
+            //this is the ties bucket.  It doesn't contain conditions similar to above because there are only 3 buckets that can be fallen into--you either fall into the wins bucket or the losses bucket, if you make it past that then the only option left is that it was a tie.
+            else {
+                ties ++;
+            }
 
         //this block creates the text for the page and displays the user's entry, the computer's generated choice, the number of wins, losses, and ties.
         var html = 
@@ -41,7 +44,12 @@ document.onkeyup = function(event){
 
         //this takes the var html's block of code and injects it onto the page.
         document.querySelector("#game").innerHTML = html;
+        }
 
+        //this alerts the player that they are not using a valid key.
+        else {
+            alert("Please press 'r', 'p', or 's' to play.");
+        }
     }
 
     //this invokes the logic of the game, but is only called once a key is pressed.
